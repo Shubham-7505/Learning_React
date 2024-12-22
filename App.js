6af6a705c55b192e2c0a573260669173b0d16c2f
Shubham Kumar <shubham07pal@gmail.com>
2024-12-22 14:43:08 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// const heading = React.createElement(
-//     "h1",
-//     {id:"heading"},
-//     "Hello, My name is Shubham Kumar."
-// );
 
-//React.createElement is used to create the react elements which 
-// will create a javascript object.
-// render method will convert this object into HTML tags.
-const parent = React.createElement("div",
-  {id:"parent"}, 
-  [React.createElement("div", {id:"child1"},
-    [React.createElement("h1", {}, "I am h1 tag form child 1"),
-     React.createElement("h2", {}, "I am h2 tag form child 1")
-    ]
-  ),                
-  React.createElement("div", {id:"child2"},
-    [React.createElement("h1", {}, "I am h1 tag form child 2"),
-     React.createElement("h2", {}, "I am h2 tag form child 2")
-    ]
-  )
-]
-);
+const heading = React.createElement("h1", {id: "heading"}, 
+  "Namaste React"
+); // this is the core react, to simplify this more jsx was developed
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
